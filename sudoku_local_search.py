b0bc54430d8945fcdac33636b093a82739b3a598
Andrew Antonow <andreika_antonov@mail.ru>
2024-06-02 14:56:09 +0300

    Problem was solved. Commit includes output.

--- sudoku_local_search.py
+++ sudoku_local_search.py
@@ -1,22 +1,11 @@
 import random
-import sys
 import copy
 
 
 from sudoku import Sudoku
 from icecream import ic
 
-
-import sudoku_functions as funcs
-from timing import timing_decorator
-
-
-
 random.seed(42)
-
-MAX_ITER = 1000000
-MAX_RESTART = 10
-
 class Puzzle:
     """A class that represents sudoku."""
 
@@ -27,7 +16,8 @@ class Puzzle:
         :param board: Sudoku 3x3.
         :type board: list[int]
         """
-        self.board = board
+        self.board = copy.deepcopy(board)
+        self.init_board = copy.deepcopy(board)
         self.blanks = self._get_blanks()
         self.blank_fillers = self._calculate_blank_cell_fillers()
 
@@ -135,7 +125,7 @@ class Puzzle:
         sq_j = j // 3
 
         sq = self.board[sq_i: sq_i + 3][sq_j: sq_j + 3]
-
+        sq = [row[sq_j: sq_j + 3] for row in self.board[sq_i: sq_i + 3]]
         for row in sq:
             for el in row:
                 if el == el_to_check:
@@ -143,20 +133,143 @@ class Puzzle:
 
         return square_collisions_counter
 
-    @timing_decorator
-    def calc_fitness(self)-> int:
+    # @timing_decorator
+    def calc_fitness(self, board_in=None)-> int:
         """
         Method to calculate goal function. Number of conflicts in this case.
         """
-        fitness = 0
-        for i, row in enumerate(self.board):
-            for y, _ in enumerate(row):
-                fitness += self._row_rule(i, y)
-                fitness += self._col_rule(i, y)
-                fitness += self._square_rule(i, y)
+        if board_in == None:
+            board = self.board
+        else: 
+            board = copy.deepcopy(board_in)
+        collisions = 0
+        
+        # Подсчет коллизий в строках
+        for row in board:
+            collisions += len(row) - len(set(row))
+        
+        # Подсчет коллизий в столбцах
+        for col in range(len(board)):
+            column_values = [board[row][col] for row in range(len(board))]
+            collisions += len(column_values) - len(set(column_values))
+        
+        # Подсчет коллизий в квадратах 3x3
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                square_values = []
+                for row in range(3):
+                    for col in range(3):
+                        square_values.append(board[i + row][j + col])
+                collisions += len(square_values) - len(set(square_values))
+        
+        return collisions
 
-        # All collisions will be counted twice.
-        return fitness / 2
+    def gen_table(self) -> list[int]:
+        """
+        A method of filling a table with values that preserves its solvability.
+        """
+        valid_values = []
+        for i in range(1, 10):
+            count = 0
+            for row in self.init_board:
+                for cell in row:
+                    if cell == i:
+                        count += 1
+            valid_values +=  [i] * (9 - count)
+        random.shuffle(valid_values)
+
+        for x, row in enumerate(board):
+            for y, cell in enumerate(row):
+                if cell == 0:
+                    cell = random.randint(1, 9)
+                    self.board[x][y] = valid_values[0]
+                    valid_values.remove(valid_values[0])
+
+        return self.board
+    
+    def show(self) -> str:
+        """Method to print and return table in ASCII format.
+        
+        :rtype: str
+        """
+        table = ''
+        cell_length = len(str(9))
+        format_int = '{0:0' + str(cell_length) + 'd}'
+        for i, row in enumerate(self.board):
+            if i == 0:
+                table += ('+-' + '-' * (cell_length + 1) *
+                        3) * 3 + '+' + '\n'
+            table += (('| ' + '{} ' * 3) * 3 + '|').format(*[format_int.format(
+                x) if x != 0 else ' ' * cell_length for x in row]) + '\n'
+            if i == 9 - 1 or i % 3 == 3 - 1:
+                table += ('+-' + '-' * (cell_length + 1) *
+                        3) * 3 + '+' + '\n'
+        print(table)
+        return table
+    
+
+    def get_cell(self, cell: tuple[int, int])-> int:
+        return self.board[cell[0]][cell[1]]
+    
+    def _possible_to_swap(self, cell_1: tuple[int, int], cell_2: tuple[int, int])-> bool:
+        """
+        An internal method to evaluate the ability to rearrange.
+
+        :param cell_1: First cell.
+        :type cell_1: tuple[int, int]
+        :param cell_2: Second cell.
+        :type cell_2: tuple[int, int]
+        :return: If the permutation is valid, it returns True. Otherwise False.
+        :rtype: bool 
+        """
+        cell_1_values = self.blank_fillers[cell_1[0]][cell_1[1]]
+        cell_2_values = self.blank_fillers[cell_2[0]][cell_2[1]]
+
+        get_cell_1 = self.get_cell(cell_1)
+        get_cell_2 = self.get_cell(cell_2)
+
+        num_1 = [i for i in range(1, 10) if cell_1_values[i - 1]]
+        num_2 = [i for i in range(1, 10) if cell_2_values[i - 1]]
+
+        left = cell_2_values[self.get_cell(cell_1) - 1]
+
+        right = cell_1_values[self.get_cell(cell_2) - 1]
+
+        if cell_2_values[self.get_cell(cell_1) - 1] and cell_1_values[self.get_cell(cell_2) - 1]:
+            return True
+        
+        return False
+
+    def find_swap(self)-> list[tuple[int, int], int]:
+        """
+        A method for finding the best permutation.
+
+        :return: List with new board and new goal.
+        :rtype: list[list[list[int]], int]
+        """
+
+        def swap_cells(board: list[int], first: tuple[int, int], second: tuple[int, int])-> list[int]:
+            new_board = copy.deepcopy(board)
+            new_board[first[0]][first[1]] = board[second[0]][second[1]]
+            new_board[second[0]][second[1]] = board[first[0]][first[1]]
+            return new_board
+
+        best_board = self.board
+        best_fitness = self.calc_fitness()
+        for anchor in self.blanks:
+            for stone in self.blanks:
+                new_fitness = best_fitness
+                if self._possible_to_swap(anchor, stone):
+                    new_board = swap_cells(self.board, anchor, stone)
+                    new_fitness = self.calc_fitness(board_in=new_board)
+
+                if new_fitness < best_fitness:
+                    best_fitness = new_fitness
+                    best_board = new_board
+                
+        return [best_board, best_fitness]
+
+
 
 if __name__ == "__main__":
     no_solution = False
@@ -173,51 +286,47 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 0, 0, 7, 0, 9],
     ]
 
-    puzzle = Sudoku(3, 3, board=board)
-    puzzle.show_full()
-    # puzzle.solve().show_full()
-
-    blanks = funcs.get_blanks(board)
-    blank_count = len(blanks)
-    are_blanks_filled = [False for _ in range(blank_count)]
-    blank_fillers = funcs.calculate_blank_cell_fillers(blanks, board)
-
-
-    # ic.disable()
-    ic(blanks)
-    ic(blank_fillers)
-    ic(len(blank_fillers) * len(blank_fillers[0]) * len(blank_fillers[0][0]))
-
-
-    filled_board = funcs.gen_table(board)
-    ic(funcs.count_unique(filled_board))
-    my_puzzle = Sudoku(3, 3, board=filled_board)
-
-    my_puzzle.show_full()
+    reference = Sudoku(3, 3, board=board)
+    reference.show_full()
+    reference.solve().show_full()
 
     ic.disable()
-    fitness = funcs.calc_fitness(filled_board, 0, 0)
-    ic(fitness)
+    my_sudoku = Puzzle(board)
+    my_sudoku.show()
+    ic(my_sudoku.blanks)
+    ic(my_sudoku.blank_fillers)
+
+    ic.enable()
+    my_sudoku.gen_table()
+    my_sudoku.show()
+
+    my_sudoku.gen_table()
+    goal = 500
+
+    while goal > 0:
+        if goal != 500:
+            random.seed(None)
+            my_sudoku.gen_table()
+            goal = my_sudoku.calc_fitness()
 
 
-    while fitness > 0 and not no_solution:
-        cell_value = filled_board[cell[0]][cell[1]]
-        for cell in blanks:
-            swap, new_fitness = funcs.find_cell_to_swap(filled_board, cell, fitness, blanks)
-            if new_fitness == fitness:
+        no_solution = False
+        count_iter = 0
+
+        while goal > 0 and not no_solution:
+            new_board, new_goal = my_sudoku.find_swap()
+            if new_goal == goal:
                 no_solution = True
-
+            
+            goal = new_goal
+            my_sudoku.board = new_board
             count_iter += 1
-            fitness = new_fitness
-            filled_board[cell[0]][cell[1]] = filled_board[swap[0]][swap[1]]
-            filled_board[swap[0]][swap[1]] = cell_value
 
-
-
-    if no_solution:
-        print("Решение не было найдено. Остановка в локальном минимуме или на плато.")
-        sys.exit(0)
-
-    print(f"Решение было найдено за {count_iter}")
-    solved = Sudoku(3, 3, board=filled_board)
-    solved.show_full()
+        if no_solution:
+            ic("Решение не было найдено. Остановка в локальном минимуме или на плато.")
+            ic(f"Целевая функция: {goal}.")
+            ic("Судоку:")
+            if ic.enabled:
+                my_sudoku.show()
+        else:
+            print(f"Решение было найдено за {count_iter}")
